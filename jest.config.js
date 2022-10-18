c764ffbd378f5b895a3cf132d68823f55f07e772
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-10-18 11:31:39 -0300

    refactor: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,11 @@
 module.exports = {
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/**/index.ts',
-    '!**/*.d.ts'
+    '<rootDir>/src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/tests/e2e/cypress'
-  ],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'identity-obj-proxy'
+    '.+\\.ts$': 'ts-jest'
   }
 }
